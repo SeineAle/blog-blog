ab4d8c15ae22db8129f340660b613f9918e95ba0
SeineAle <paras.jain.real@gmail.com>
2024-06-03 09:11:56 +0530

    added cors compattibility to backend and redeployed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-
+import { cors } from 'hono/cors'
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
 
@@ -8,11 +8,12 @@ const app = new Hono<{
 		DATABASE_URL: string,
 		JWT_SECRET: string,
 	},
-  Variables: {
-    userId : string,
-  }
+	Variables: {
+		userId : string,
+	}
 }>();
 
+app.use('/*', cors());
 app.route('api/v1/user', userRouter);
 app.route('api/v1/blog', blogRouter);
     
